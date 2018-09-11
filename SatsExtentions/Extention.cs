cadb561768c9bd1c73b2dfec4bf1f91e4662a1b9
Satish Patil <satsvelke@gmail.com>
2018-09-11 16:58:07 +0530

    Add files via upload

---
+++ SatsExtentions/Extention.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+using System.Web.Script.Serialization;
+
+namespace Sats.Extentions
+{
+    public static class Extention
+    {
+        public static string ToJson(this object o)
+        {
+            try
+            {
+
+                JavaScriptSerializer serialize = new JavaScriptSerializer();
+
+               
+               
+                 
+                    return serialize.Serialize(o);
+                
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        public static string ToFirstLetterCapital(this string str)
+        {
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(str))
+                {
+                    str = str.TrimEnd().TrimStart();
+                    return str.Substring(0, 1).ToUpper() + str.Substring(1, str.Length - 1);
+                }
+                else
+                {
+                    return str;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static string ToFirstOfEveryWord(this string str)
+        {
+            try
+            {
+                string newSentence = string.Empty;
+                if (!string.IsNullOrWhiteSpace(str))
+                {
+                    string[] arrayOfWords = str.TrimStart().TrimEnd().Split(' ');
+
+                    for (int i = 0; i < arrayOfWords.Length; i++)
+                    {
+                        newSentence += arrayOfWords[i].Substring(0, 1).ToUpper() + arrayOfWords[i].Substring(1, arrayOfWords[i].Length - 1) + " ";
+                    }
+
+                    return newSentence.TrimEnd();
+                }
+                else
+                {
+                    return str;
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static T ToJsonObject<T>(this string s)
+        {
+            try
+            {
+                JavaScriptSerializer serialize = new JavaScriptSerializer();
+                return serialize.Deserialize<T>(s);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        public static List<List<T>> SplitData<T>(this List<T> s, int splitInto)
+        {
+
+            //if (s.Count < splitInto || splitInto == 1 || splitInto == 0)
+            //    throw new ArgumentException("split count is greater than list Count or split count is 0 or 1");
+
+
+            List<List<T>> st = new List<List<T>>();
+
+            double divisor = Convert.ToDouble(s.Count) / Convert.ToDouble(splitInto);
+
+
+            divisor = Convert.ToInt32(Math.Round(divisor));
+
+
+            List<T> newSt = new List<T>();
+            for (int i = 0; i < (s.Count); i += Convert.ToInt32(divisor))
+            {
+                newSt = s.GetRange(i, Convert.ToInt32(divisor));
+                s.RemoveRange(i, Convert.ToInt32(divisor));
+                st.Add(newSt);
+            }
+
+           
+
+            return st;
+        }
+
+
+    }
+
+
+}
+
+
---
+++ SatsExtentions/SqlFunctions.cs
@@ -0,0 +1,238 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace Sats.Ado.Net
+{
+    public abstract partial class AdoNet : IDisposable
+    {
+        string constring = string.Empty;
+        DataSet dataSet = new DataSet();
+        DataTable datatable = new DataTable();
+        bool disposed = false;
+        SafeHandle handle = new SafeFileHandle(IntPtr.Zero, true);
+
+
+        public AdoNet(string connString)
+        {
+            constring = connString; 
+        }
+
+        #region Private Methods
+
+        private SqlCommand SqlCommand(string ProcedureName, SqlParameter[] parameters, SqlConnection sqlConn)
+        {
+            using (var sqCommand = new SqlCommand(ProcedureName, sqlConn))
+            {
+                sqCommand.CommandType = CommandType.StoredProcedure;
+                sqCommand.Parameters.AddRange(parameters);
+                return sqCommand;
+            }
+
+        }
+
+
+        private SqlCommand SqlCommand(string ProcedureName, object o, SqlConnection sqlConn)
+        {
+            using (var sqCommand = new SqlCommand(ProcedureName, sqlConn))
+            {
+                sqCommand.CommandType = CommandType.StoredProcedure;
+
+                PropertyInfo[] properties = o.GetType().GetProperties().ToArray();
+                foreach (var property in properties)
+                {
+                    sqCommand.Parameters.AddWithValue(property.Name, property.GetValue(o));
+                }
+
+                return sqCommand;
+            }
+
+        }
+        #endregion Private Methods
+
+
+        public void ExecuteProcedure(string ProcedureName, SqlParameter[] parameters)
+        {
+            try
+            {
+                using (var sqlConn = new SqlConnection(constring))
+                {
+                    sqlConn.Open();
+                    var sqlCommand = SqlCommand(ProcedureName, parameters, sqlConn);
+                    using (sqlCommand)
+                    {
+                        sqlCommand.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+        public void ExecuteProcedure(string ProcedureName, object o)
+        {
+            try
+            {
+                using (var sqlConn = new SqlConnection(constring))
+                {
+                    sqlConn.Open();
+                    var sqlCommand = SqlCommand(ProcedureName, o, sqlConn);
+                    using (sqlCommand)
+                    {
+                        sqlCommand.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+
+        public DataTable GetDataTable(string ProcedureName, SqlParameter[] parameters)
+        {
+
+            try
+            {
+                using (var sqlConn = new SqlConnection(constring))
+                {
+                    sqlConn.Open();
+
+                    var sqlCommand = SqlCommand(ProcedureName, parameters, sqlConn);
+
+                    using (sqlCommand)
+                    {
+                        using (var sqlDataAdapter = new SqlDataAdapter(sqlCommand))
+                        {
+                            sqlDataAdapter.Fill(datatable);
+                        }
+                    }
+                }
+
+                return datatable;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public DataTable GetDataTable(string ProcedureName, object o)
+        {
+
+            try
+            {
+                using (var sqlConn = new SqlConnection(constring))
+                {
+                    sqlConn.Open();
+
+                    var sqlCommand = SqlCommand(ProcedureName, o, sqlConn);
+
+                    using (sqlCommand)
+                    {
+                        using (var sqlDataAdapter = new SqlDataAdapter(sqlCommand))
+                        {
+                            sqlDataAdapter.Fill(datatable);
+                        }
+                    }
+
+                }
+
+                return datatable;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public DataSet GetDataSet(string ProcedureName, SqlParameter[] parameters)
+        {
+
+            try
+            {
+                using (var sqlConn = new SqlConnection(constring))
+                {
+                    sqlConn.Open();
+
+                    var sqlCommand = SqlCommand(ProcedureName, parameters, sqlConn);
+
+                    using (sqlCommand)
+                    {
+                        using (var sqlDataAdapter = new SqlDataAdapter(sqlCommand))
+                        {
+                            sqlDataAdapter.Fill(dataSet);
+                        }
+                    }
+
+                }
+
+                return dataSet;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public DataSet GetDataSet(string ProcedureName, object o)
+        {
+
+            try
+            {
+                using (var sqlConn = new SqlConnection(constring))
+                {
+                    sqlConn.Open();
+
+                    var sqlCommand = SqlCommand(ProcedureName, o, sqlConn);
+
+                    using (sqlCommand)
+                    {
+                        using (var sqlDataAdapter = new SqlDataAdapter(sqlCommand))
+                        {
+                            sqlDataAdapter.Fill(dataSet);
+                        }
+                    }
+
+                }
+
+                return dataSet;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+            {
+                handle.Dispose();
+                datatable.Dispose();
+                dataSet.Dispose();
+            }
+
+
+            disposed = true;
+        }
+    }
+}
